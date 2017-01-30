1f6103238d944b88823a3d721739e77f536b7628
William Scanlon <wjs.scanlon@gmail.com>
2017-01-30 12:49:10 -0500

    Initial support for sensors

--- simplisafe.py
+++ simplisafe.py
@@ -1,8 +1,10 @@
-import requests
 import json
 import sys
 import uuid
 import argparse
+import logging
+
+import requests
 
 LOGIN_URL = 'https://simplisafe.com/mobile/login/'
 LOGOUT_URL = 'https://simplisafe.com/mobile/logout'
@@ -10,6 +12,9 @@ LOCATIONS_URL = 'https://simplisafe.com/mobile/$UID$/locations'
 DASHBOARD_URL = 'https://simplisafe.com/mobile/$UID$/sid/$LID$/dashboard'
 EVENTS_URL = 'https://simplisafe.com/mobile/$UID$/sid/$LID$/events'
 STATE_URL = 'https://simplisafe.com/mobile/$UID$/sid/$LID$/set-state'
+ALL_STATES = {}
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class SimpliSafe():
@@ -24,6 +29,10 @@ class SimpliSafe():
         self.location = None
         self.username = username
         self.password = password
+        self.temperature = None
+        self.co = None
+        self.flood = None
+        self.fire = None
 
         # Create a requests session to persist the cookies
         self.session = requests.session()
@@ -34,18 +43,12 @@ class SimpliSafe():
         if debug:
             self.debug = True
 
-    def abort(self, msg):
-        print("Error: %s" % msg)
-        print("Aborting and Logging Out.")
-        self.logout()
-        sys.exit()
-
     def set_state(self, state):
         self.login()
 
         if state not in ('home', 'away', 'off'):
-            self.abort("State must be 'home', 'away', \
-                       or 'off'. You tried '%s'." % state)
+            error = "State must be 'home', 'away', or 'off'. You tried '%s'." % state
+            _LOGGER.error(error)
 
         state_data = {
             'state': state,
@@ -83,14 +86,15 @@ class SimpliSafe():
         return self.uid
 
     def get_dashboard(self):
+        self.login()
 
         if not self.uid:
-            self.abort("You tried to get dashboard \
-                       without first having a User ID set.")
+            error = "You tried to get dashboard without first having a User ID set."
+            _LOGGER.error(error)
 
         if not self.location:
-            self.abort("You tried to get dashboard \
-                       without first having a location set.")
+            error = "You tried to get dashboard without first having a location set."
+            _LOGGER.error(error)
 
         dashboard_data = {
             'no_persist': '0',
@@ -100,16 +104,25 @@ class SimpliSafe():
         URL = DASHBOARD_URL.replace('$UID$', self.uid)
         URL = URL.replace('$LID$', self.location)
         response = self.session.post(URL, data=dashboard_data)
+        json_state = json.loads(response.text)
+        monitoring_sensors = json_state.get("location").get("monitoring")
+        self.temp = monitoring_sensors.get("freeze").get("temp")
+        self.co = monitoring_sensors.get("recent_co").get("text")
+        self.flood = monitoring_sensors.get("recent_flood").get("text")
+        self.fire = monitoring_sensors.get("recent_fire").get("text")
+        self.alarm = monitoring_sensors.get("recent_alarm").get("text")
 
         if self.debug:
             print("Dashboard Response: %s" % response.text)
+
+        self.logout()
 
     def get_location(self):
         self.login()
 
         if not self.uid:
-            self.abort("You tried to get location without \
-                       first having a User ID set.")
+            error = "You tried to get location without first having a User ID set."
+            _LOGGER.error(error)
 
         location_data = {
             'no_persist': '0',
@@ -129,10 +142,35 @@ class SimpliSafe():
 
         self.logout()
 
+    def get_events(self):
+        self.login()
+
+        if not self.uid:
+            error = "You tried to get events without first having a User ID set."
+            _LOGGER.error(error)
+
+        event_data = {
+            'no_persist': '0',
+            'XDEBUG_SESSION_START': 'session_name',
+        }
+
+        URL = EVENTS_URL.replace('$UID$', self.uid)
+        response = self.session.post(URL, data=event_data)
+        response_object = json.loads(response.text)
+
+        if self.debug:
+            print("Event Response: %s" % response.text)
+
+        #locations = response_object['locations']
+        #self.location = list(locations)[0]
+        #self.state = locations[self.location]['system_state']
+
+        self.logout()
+
     def login(self):
 
         if not self.username or not self.password:
-            sys.exit("You must provide a username and password.")
+            _LOGGER.error("You must provide a username and password.")
 
         login_data = {
             'name': self.username,
@@ -169,6 +207,8 @@ if __name__ == "__main__":
     parser.add_argument('--state', help='Alarm State', required=False)
     parser.add_argument('--debug', help='Output debugging',
                         required=False, default=False, action="store_true")
+    parser.add_argument('--dashboard', help='Display dashboard',
+                        required=False, default=False, action="store_true")
 
     args = vars(parser.parse_args())
 
@@ -178,10 +218,18 @@ if __name__ == "__main__":
 
     s = SimpliSafe(args['username'], args['password'], args['debug'])
 
-    s.get_location()
     s.login()
+    s.get_location()
+    s.get_dashboard()
+    s.get_events()
     if args['state'] is not None:
         s.set_state(args['state'].lower())
 
     print("System State: %s" % s.get_state())
+    if args['dashboard'] is not None:
+        print("Temp: " + s.temp)
+        print("CO: " + s.co)
+        print("Fire: " + s.fire)
+        print("Flood: " + s.flood)
+        print("Alarm: " + s.alarm)
     s.logout()
